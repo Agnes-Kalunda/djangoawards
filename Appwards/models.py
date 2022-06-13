286651b792061a06e2b658d6cdc2af7d0c9ed744
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-06-13 18:44:16 +0300

    registered models in admin.py

--- Appwards/models.py
+++ Appwards/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
-    profile_pic = models.ImageField(upload='images/')
+    profile_pic = models.ImageField(upload_to='images/')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500)
     information = models.TextField(max_length=500)
