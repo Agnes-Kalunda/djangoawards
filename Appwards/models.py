139497883a3bbd7383515f6564cf4a7a2503acb9
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-06-13 21:17:13 +0300

    installed cloudinary

--- Appwards/models.py
+++ Appwards/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 # Create your models here.
 class Profile(models.Model):
-    profile_pic = models.ImageField(upload_to='images/')
+    profile_pic = CloudinaryField('image')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500)
     information = models.TextField(max_length=500)
@@ -13,9 +15,10 @@ class Profile(models.Model):
         return self.save()
     def delete_profile(self):
         return self.delete()
+
 class Project(models.Model):
     title = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='images/')
+    image = CloudinaryField('image')
     description = models.CharField(max_length=300)
     link = models.URLField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -34,29 +37,23 @@ class Project(models.Model):
     def search_project(cls, search_term):
         search_project = cls.objects.filter(title=search_term)
         return search_project
-
-
 class Comments(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
     text = models.CharField(max_length=200)
     project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
-
-
     def __str__(self):
         return self.user
-
+    
     def save_comments(self):
         self.save()
-
+    
     def delete_comments(self):
         self.delete()
-
-
     @classmethod 
     def all_comments(cls, id):
         comments = cls.objects.filter(project_id = id)
         return comments
-
+    
 class Rating(models.Model):
     project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- mawards/settings.py
+++ mawards/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Appwards',
     'bootstrap4',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -124,7 +126,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
+cloudinary.config( 
+  cloud_name = "dd206fmmw", 
+  api_key = "423815319196181", 
+  api_secret = "ydJi_a5TIETAFMMogbI4rKDWVek",
+#   secure = true
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
